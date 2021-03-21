52764118f85cd200f3a57e0af6eb0d36be118578
JoshuaMK <joshuamkw2002@gmail.com>
2021-03-21 15:36:35 -0500

    Update 64 bit types to *int64_t, relocate to source/

--- source/types.h
+++ source/types.h
@@ -5,11 +5,11 @@
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
-typedef uintmax_t u64;
+typedef uint64_t u64;
 typedef int8_t s8;
 typedef int16_t s16;
 typedef int32_t s32;
-typedef intmax_t s64;
+typedef int64_t s64;
 typedef float f32;
 typedef double f64;
 typedef volatile s8 vs8;

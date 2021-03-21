a783f0a481805f701f65b4e20f13ec0531fadd1f
JoshuaMK <joshuamkw2002@gmail.com>
2021-03-21 15:29:52 -0500

    Use stdint, make C types follow standard

--- src/src-code/system/types.h
+++ src/src-code/system/types.h
@@ -1,32 +1,35 @@
-#ifndef TYPES_H
-#define TYPES_H
+#pragma once
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned long u32;
-typedef unsigned long long u64;
-typedef char s8;
-typedef short s16;
-typedef long s32;
-typedef long long s64;
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uintmax_t u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef intmax_t s64;
 typedef float f32;
 typedef double f64;
-typedef volatile s64 vs64;
-typedef volatile s32 vs32;
-typedef volatile s16 vs16;
 typedef volatile s8 vs8;
-typedef volatile u64 vu64;
-typedef volatile u32 vu32;
-typedef volatile u16 vu16;
+typedef volatile s16 vs16;
+typedef volatile s32 vs32;
+typedef volatile s64 vs64;
 typedef volatile u8 vu8;
-typedef volatile f64 vf64;
+typedef volatile u16 vu16;
+typedef volatile u32 vu32;
+typedef volatile u64 vu64;
 typedef volatile f32 vf32;
+typedef volatile f64 vf64;
 
 typedef unsigned long size_t;
-typedef int BOOL;
 
-#define NULL 0
-#define TRUE 1
-#define FALSE 0
-
+#ifdef __cplusplus
+#define NULL nullptr
+#else
+#define NULL ((void*)0)
+#define bool int
+#define true 1
+#define false 0
 #endif
